e0c2563ca8ae015565186a3723a8f19d89a9fcc9
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-19 00:00:53 +0530

    Added: Regex to check first name capital with atleast 3 chars

--- src/main/java/com/bridgelabz/UserValidator.java
+++ src/main/java/com/bridgelabz/UserValidator.java
@@ -1,3 +1,7 @@
 package com.bridgelabz;
 public class UserValidator {
+    public static final String namePattern = "^[A-Z]{1}[a-z]{2,}";
+    public static boolean userDataValidation(String dataFromTest,String pattern){
+        return dataFromTest.matches(pattern);
+    }
 }
